fc161d187627695f9578556ab54b01db5096459b
Wei Lai <wlai@liveperson.com>
2024-11-25 08:45:41 -0500

    add redis

--- app/api/pixel/route.ts
+++ app/api/pixel/route.ts
@@ -1,7 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { db, TrackingData } from '@/lib/db';
 
+import { createClient } from 'redis';
+
+
+
+
 export async function GET(request: NextRequest) {
+
+
+  const client = createClient({
+    url: process.env.REDIS_URL
+  });
+  client.on('error', err => console.log('Redis Client Error', err));
+  await client.connect();
+  console.log('Client Connected')
+
+
   const query = request.nextUrl.searchParams.toString();
   const userAgent = request.headers.get('user-agent') || 'Unknown';
   const ip = request.ip || 'Unknown';
