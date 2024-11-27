6cfc5faa923b4ae1de1085d87c7d639bf1416f68
Wei Lai <wlai@liveperson.com>
2024-11-27 13:33:22 -0500

    add logging

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import { createClient } from 'redis';
+import { createClient } from "redis";
 
 export interface TrackingData {
   timestamp: string;
@@ -8,48 +8,53 @@ export interface TrackingData {
 }
 
 const client = createClient({
-  url: process.env.REDIS_URL
+  url: process.env.REDIS_URL,
 });
 
 class InMemoryDB {
   private storage: Map<string, TrackingData>;
-  
 
   constructor() {
     this.storage = new Map();
-    client.on('error', err => console.log('Redis Client Error', err));
-    client.connect().then(() => {
-      console.log('Client Connected');
-    }).catch((err) => {
-      console.error('Failed to connect to Redis:', err);
-    });
+    client.on("error", (err) => console.log("Redis Client Error", err));
+    client
+      .connect()
+      .then(() => {
+        console.log("Client Connected");
+      })
+      .catch((err) => {
+        console.error("Failed to connect to Redis:", err);
+      });
   }
 
   async addEntry(data: TrackingData) {
-    console.log('add data:', data);
-    const id = Date.now().toString();
-    this.storage.set(id, data);
-    await client.set(id, JSON.stringify(data));
-    console.log('data added:', data);
-
+    try {
+      console.log("add data:", data);
+      const id = Date.now().toString();
+      this.storage.set(id, data);
+      const result = await client.set(id, JSON.stringify(data));
+      console.log("data added:", result);
+    } catch (error) {
+      console.error("error adding redis data", error);
+    }
   }
 
-  async convertToMap(promise:Promise<Record<string, TrackingData>>) {
+  async convertToMap(promise: Promise<Record<string, TrackingData>>) {
     const record = await promise; // Await the Promise to get the Record object
     const map = new Map(Object.entries(record)); // Convert the Record to a Map
     return map;
-}
+  }
 
- convertTimestampsToLocalTime = (array:TrackingData[]) => {
-  return array.map((item) => ({
-    ...item, // Keep other properties unchanged
-    timestamp: new Date(item.timestamp).toLocaleString(), // Convert timestamp
-  }));
-};
+  convertTimestampsToLocalTime = (array: TrackingData[]) => {
+    return array.map((item) => ({
+      ...item, // Keep other properties unchanged
+      timestamp: new Date(item.timestamp).toLocaleString(), // Convert timestamp
+    }));
+  };
 
   async getAllEntries(): Promise<TrackingData[]> {
     try {
-      const keys = await client.keys('*');
+      const keys = await client.keys("*");
       console.log(keys);
       const allData: Record<string, TrackingData> = {};
 
@@ -66,7 +71,7 @@ class InMemoryDB {
       return this.convertTimestampsToLocalTime(Array.from(map.values()));
       // return allData;
     } catch (err) {
-      console.error('Error getting all data:', err);
+      console.error("Error getting all data:", err);
       // return {};
       return [];
     }
@@ -74,4 +79,3 @@ class InMemoryDB {
 }
 
 export const db = new InMemoryDB();
-
