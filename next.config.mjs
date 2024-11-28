c93968ab0239ab8420f7aa88e227fa247681ae92
Wei Lai <wlai@liveperson.com>
2024-11-28 09:07:03 -0500

    upgrade to nextjs 15

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-      return [
-        {
-          source: "/:path*",
-          headers: [
-            {
-              key: "Cache-Control",
-              value: "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0",
-            },
-          ],
-        },
-      ];
-    },
+    // async headers() {
+    //   return [
+    //     {
+    //       source: "/:path*",
+    //       headers: [
+    //         {
+    //           key: "Cache-Control",
+    //           value: "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0",
+    //         },
+    //       ],
+    //     },
+    //   ];
+    // },
   };
 
 export default nextConfig;
